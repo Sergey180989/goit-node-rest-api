10878edffb9c6de34dd09168daa1090b87e026d8
Sergey180989 <133609830+Sergey180989@users.noreply.github.com>
2024-02-17 01:08:04 +0200

    create-2exp

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,71 @@
-import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  addContact,
+  getContactById,
+  listContacts,
+  removeContact,
+  updateContactById,
+} from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await listContacts();
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await getContactById(id);
+    if (!result) {
+      throw HttpError(404, `Not Found, Contact with id=${id} not found`);
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await removeContact(id);
+    if (!result) {
+      throw HttpError(404, `Not Found, Contact with id=${id} not found`);
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const result = await addContact(req.body);
+    res.status(201).json({
+      msg: `Success`,
+      result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  try {
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const result = await updateContactById(id, req.body);
+    if (!result) {
+      throw HttpError(404, `Not Found, Contact with id=${id} not found`);
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
