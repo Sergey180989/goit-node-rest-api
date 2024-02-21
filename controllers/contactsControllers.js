a12b4305f1c6c3bc129eaca0aedea722f9e2d8b9
Sergey180989 <133609830+Sergey180989@users.noreply.github.com>
2024-02-21 17:04:59 +0200

    add-hw

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,103 @@
-import contactsService from "../services/contactsServices.js";
+import { Contacts } from "../db/contacts.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+  updateFavoriteSchema,
+} from "../schemas/contactsSchemas.js";
+import mongoose from "mongoose";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await Contacts.find();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw HttpError(400, "Invalid contact ID");
+    }
+    const contact = await Contacts.findById(id);
+    if (!contact) {
+      throw HttpError(404, `Not Found, Contact with id=${id} not found`);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedContact = await Contacts.findByIdAndDelete(id);
+    if (!deletedContact) {
+      throw HttpError(404, `Not Found, Contact with id=${id} not found`);
+    }
+    res.status(200).json(deletedContact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const newContact = await Contacts.create(req.body);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw HttpError(400, "Invalid contact ID");
+    }
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const updatedContact = await Contacts.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedContact) {
+      throw HttpError(404, `Not Found, Contact with id=${id} not found`);
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateFavorite = async (req, res, next) => {
+  try {
+    if (!("favorite" in req.body)) {
+      throw HttpError(400, "Missing field favorite");
+    }
+    const { error } = updateFavoriteSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const updatedContact = await Contacts.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedContact) {
+      throw HttpError(404, `Not Found, Contact with id=${id} not found`);
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ db/contacts.js
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+import { MongooseError } from "../helpers/MongooseError.js";
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", MongooseError);
+
+export const Contacts = model("contacts", contactSchema);
